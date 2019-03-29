516afbe548782cbacb9d01107d4b9c4abf482908
Stephanie Purple <spurple133@gmail.com>
2019-03-29 16:33:58 +0000

    Automatically backed up by Learn

--- spec/second_challenge_spec.rb
+++ spec/second_challenge_spec.rb
@@ -1,7 +1,8 @@
-require_relative 'spec_helper'
-require_relative '../lib/second_challenge.rb'
 require 'pry'
 require './lib/pry_debugging.rb'
+require_relative 'spec_helper'
+require_relative '../lib/second_challenge.rb'
+
 
 describe "#second_challenge" do
   it "uses the .values method to collect all of the grocery items from the groceries hash" do
